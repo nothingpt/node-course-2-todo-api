fdbe4e8f283ec4b82fc7dad34275c33c9986ae0a
Nuno Santos <nsantos.pessoal@gmail.com>
2017-05-31 14:15:03 +0100

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -111,7 +111,6 @@ app.post('/users', (req, res) => {
       return user.generateAuthToken();
     })
     .then((token) => {
-      console.log('auth: ', token);
       res.header('x-auth', token).send(user);
     })
     .catch((e) => {
@@ -121,6 +120,28 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      })
+    })
+    .catch((e) => {
+      res.status(400).send();
+    })
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send()
+  }, () => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,25 +4,18 @@ const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 //const { User } = require('./../models/user');
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: 'First test todo'
-  },
-  {
-    _id: new ObjectID(),
-    text: 'Second test todo'
-  }
-];
-
 // clean the database before each test
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+// beforeEach((done) => {
+//   Todo.remove({}).then(() => {
+//     return Todo.insertMany(todos);
+//   }).then(() => done());
+// });
+
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 // describe('POST /todos', function () {
 //   it('should create a new todo', (done) => {
@@ -64,43 +57,61 @@ beforeEach((done) => {
 //       });
 //   });
 // });
+//
+// describe('GET /todos', function () {
+//   it('should get all todos', (done) => {
+//     request(app)
+//       .get('/todos')
+//       .expect(200)
+//       .expect((res) => {
+//         expect(res.body.todos.length).toBe(2);
+//       })
+//       .end(done);
+//   });
+// });
+//
+// describe('GET /todos/:id', () => {
+//   it('should return todo doc', (done) => {
+//     request(app)
+//       .get(`/todos/${todos[0]._id.toHexString()}`)
+//       .expect(200)
+//       .expect(res => {
+//         expect(res.body.todo.text).toBe(todos[0].text)
+//       })
+//       .end(done)
+//   });
+//
+//   it('should return 404 if doc not found', (done) => {
+//     var hexId = new ObjectID().toHexString();
+//
+//     request(app)
+//       .get(`/todos/${hexId}`)
+//       .expect(404)
+//       .end(done);
+//   });
+//
+//   it('should return 404 for non-object ids', (done) => {
+//     request(app)
+//       .get('/todos/123qwe')
+//       .expect(404)
+//       .end(done);
+//   });
+// });
 
-describe('GET /todos', function () {
-  it('should get all todos', (done) => {
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
     request(app)
-      .get('/todos')
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
       .expect(200)
       .expect((res) => {
-        expect(res.body.todos.length).toBe(2);
+        expect(res.body.id).toBe(users[0]._id.toHexString());
+        expect (res.body.email).toBe(users[0].email);
       })
       .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+
   });
 });
-
-describe('GET /todos/:id', () => {
-  it('should return todo doc', (done) => {
-    request(app)
-      .get(`/todos/${todos[0]._id.toHexString()}`)
-      .expect(200)
-      .expect(res => {
-        expect(res.body.todo.text).toBe(todos[0].text)
-      })
-      .end(done)
-  });
-
-  it('should return 404 if doc not found', (done) => {
-    var hexId = new ObjectID().toHexString();
-
-    request(app)
-      .get(`/todos/${hexId}`)
-      .expect(404)
-      .end(done);
-  });
-
-  it('should return 404 for non-object ids', (done) => {
-    request(app)
-      .get('/todos/123qwe')
-      .expect(404)
-      .end(done);
-  });
-})
